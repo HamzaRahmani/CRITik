df9f1d988e273eaa6d963320b505e96a1be8b304
Hamza Rahmani <mrezio450@gmail.com>
2019-11-20 21:38:53 -0500

    cleaned up code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,28 +3,9 @@ import Search from '../Search/Search'
 import './Gradient.scss'
 
 export default class Layout extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-          animationClass: 'test'
-        }
-        this.changeState = this.changeState.bind(this);
-      }
-      changeState(){
-        if(this.state.animationClass === 'test'){
-          this.setState({
-            animationClass: 'test paused'
-          });
-        }else{
-          this.setState({
-            animationClass: 'test'
-          });
-        }
-      }
     render() {
         return (
-         
-        <div className={this.state.animationClass}>
+        <div className="back">
           <div className="search">
             <Search ></Search>
           </div>
